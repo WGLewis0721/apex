7d71dc203b3eb8e15fc4ce7e981b03c4ca6d21bc
WGLewis0721 <William.GLewis17@gmail.com>
2026-09-09 16:42:22 -0500

    feat: mount APEX business model section on homepage

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ProductSite from './ProductSite';
+import BusinessModelPortal from './components/BusinessModelPortal';
 import './tokens.css';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ProductSite />
+    <BusinessModelPortal />
   </React.StrictMode>,
 );
